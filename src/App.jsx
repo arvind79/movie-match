d94e376b8fbe230e01ad9121d50cc90d1661323c
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-07-08 16:51:01 +0530

    added header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
 
   return (
     <BrowserRouter>
-    {/* <Header /> */}
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
@@ -61,7 +61,7 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    {/* <Footer /> */}
+    <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
+import useFetch from '../../../hooks/useFetch'
+
+const Trending = () => {
+  const [endPoint, setEndpoint] = useState("day")
+  const {data, loading} = useFetch(`/trending/all/${endPoint}`)
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week")
+  }
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+    </div>
+  )
+}
+
+export default Trending
