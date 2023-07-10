25956e7641000ae9245e3a1b6becf7f2f1038390
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-07-10 18:14:07 +0530

    Search Result page completed

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -6,8 +6,8 @@ import "./style.scss";
 
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
-// import MovieCard from "../../components/movieCard/MovieCard";
-// import Spinner from "../../components/spinner/Spinner";
+import MovieCard from "../../components/movieCard/MovieCard";
+import Spinner from "../../components/spinner/Spinner";
 import noResults from "../../assets/no-results.png";
 
 const SearchResult = () => {
@@ -41,10 +41,40 @@ const SearchResult = () => {
   };
 
   useEffect(() => {
+    setPageNum(1)
     fetchInitialData();
   }, [query]);
 
-  return <div className="searchResultsPage">sea</div>;
+  return <div className="searchResultsPage">
+    {loading && <Spinner initial={true} />}
+    {!loading && (
+      <ContentWrapper>
+        {data?.results?.length > 0 ? (
+          <>
+            <div className="pageTitle">
+              {`Search ${data.total_results > 1 ? "results" : "result"} of '${query}'`}
+            </div>
+            <InfiniteScroll
+              className="content"
+              dataLength={data?.results?.length || []}
+              next={fetchNextPageData}
+              hasMore={pageNum <= data?.total_pages}
+              loader={<Spinner />}
+            >
+              {data?.results.map((item, index) => {
+                if(item.media_type === "person") return
+                return (
+                  <MovieCard key={index} data={item} fromSearch={true} />
+                )
+              })}
+            </InfiniteScroll>
+          </>
+        ) : (
+          <span className="resultNotFound">Sorry, Result Not Found!</span>
+        )}
+      </ContentWrapper>
+    )}
+  </div>;
 };
 
 export default SearchResult;
